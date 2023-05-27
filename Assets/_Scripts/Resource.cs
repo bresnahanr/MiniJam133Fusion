ff775eba73d9b75f0f01fa17b2a42f62d472d3fd
Daniel Brunelle <daniel.brunelle@me.com>
2023-05-26 20:43:17 -0600

    Instantiate GameState

--- Assets/_Scripts/Resource.cs
+++ Assets/_Scripts/Resource.cs
@@ -6,6 +6,13 @@ public class Resource
 {
 	private int _currentAmount = 0;
 
+	public ResourceType ResourceType { get; private set; }
+
+	public Resource(ResourceType type)
+	{
+		ResourceType = type;
+	}
+
 	public int Value => _currentAmount;
 
 	private bool CheckFunds(int amount)
@@ -18,7 +25,6 @@ public class Resource
 	    _currentAmount += amount;
     }
 
-    
     public bool Subtract(int amount)
     {
 	    if (CheckFunds(amount))
@@ -26,8 +32,12 @@ public class Resource
 		    _currentAmount -= amount;
 		    return true;
 	    }
-
 	    return false;
     }
+}
 
+public enum ResourceType
+{
+	Money,
+	Uranium,
 }
