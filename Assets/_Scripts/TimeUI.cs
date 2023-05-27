82691949241d2677fc8699c78414206e7697621a
Ryan Bresnahan <r.bresnahan@hotmail.com>
2023-05-26 19:14:48 -0600

    Added TextMeshPro to project and TimeUI script to update Time and Date UI fields

---
+++ Assets/_Scripts/TimeUI.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/** 
+ * TimeUI
+ * 
+ * A Unity class that updates the Time and Date elements on the UI canvas.
+ * Displayed time and day displayed correlates to the TimeController class tick system.
+ * 
+ * Example Usage:
+ * 1. Attach this script to a GameObject such as the canvas in your Unity scene.
+ * 2. Drag the "Time" TextMeshPro object from the heirarchy and drop it in the inspector.
+ * 3. Do the same for the "Date" TextMeshPro object.
+**/
+
+public class TimeUI : MonoBehaviour
+{
+    public TMP_Text timeText;
+    public TMP_Text dateText;
+
+	// Update the text before subscriber events take place
+	private void Start()
+	{
+		UpdateTime();
+		UpdateDay();
+	}
+
+	private void OnEnable()
+	{
+		TimeController.OnMinuteChanged += UpdateTime;
+		TimeController.OnDayChanged += UpdateDay;
+	}
+
+	private void OnDisable()
+	{
+		TimeController.OnMinuteChanged -= UpdateTime;
+		TimeController.OnDayChanged -= UpdateDay;
+	}
+
+	private void UpdateTime()
+	{
+		// Displays in scene as "00:00"
+		timeText.text = $"{TimeController.Hour:00}:{TimeController.Minute:00}";
+	}
+
+	private void UpdateDay()
+	{
+		// Displays in UI as "Day: 0"
+		dateText.text = $"Day: {TimeController.Day}";
+	}
+}
