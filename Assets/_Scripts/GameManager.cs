981000319e793187b41d260604ded7caf192761c
Ryan Bresnahan <r.bresnahan@hotmail.com>
2023-05-27 11:59:09 -0600

    Add more icons and initial game over script

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,5 +10,16 @@ public class GameManager : MonoBehaviour
     {
         GameState.Money = new Resource(ResourceType.Money);
         GameState.Uranium = new Resource(ResourceType.Uranium);
+    }
+
+    public void Restart()
+	{
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+    public void QuitGame()
+	{
+        Debug.Log("Quitting Game");
+        Application.Quit();
     }
 }
---
+++ Assets/_Scripts/GameOverUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+	public GameObject ui;
+
+	private void Awake()
+	{
+		ui.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+}
