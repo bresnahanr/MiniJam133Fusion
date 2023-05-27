6c82231c492ecb12cac2127f650de6cc9fe8aa54
Daniel Brunelle <daniel.brunelle@me.com>
2023-05-26 19:56:31 -0600

    Add script foundations

---
+++ Assets/_Scripts/GameState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public static class GameState
+{
+    public static Resource Money;
+    public static Resource Uranium;
+}
---
+++ Assets/_Scripts/Worker.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worker : MonoBehaviour
+{
+    [SerializeField] private GameObject destination;
+    [SerializeField] private GameObject lab;
+    
+    
+}
